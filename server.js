8e258e30349c3474e70424c83b190ba8f621b858
Catalin <csiegling@gmail.com>
2022-07-20 21:52:03 -0500

    finished template

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = 8000
+const TestModel = require('./models/schema')
 
-let db,
-    dbConnectionString = process.env.DB_STRING,
-    dbName = 'sample_mflix',
-    collection
+//connection async function
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_STRING,
+            { useNewUrlParser: true })
+        console.log(`Connected to database: ${mongoose.connection.name}`)
+    } catch (err) {
+        console.log(`Failed to connect`, err)
+    }
+}
 
-MongoClient.connect(dbConnectionString)
-    .then(client => {
-        console.log(`Connected to Database`)
-        db = client.db(dbName)
-        collection = db.collection('movies')
-    })
+connectDB()
 
+//Middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
 app.get('/', async (request, response) => {
     try {
-        response.render('index.ejs')
+        const content = await TestModel.find()
+        console.log(content);
+        response.render('index.ejs',{contentKey: content})
     } catch (error) {
-        response.status(500).send({message: error.message})
+        response.status(500).send({ message: error.message })
     }
 })
 
 
-//PORT = 8000
+//PORT = 8000 stored in .env
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port`)
+    console.log(`Server is running on port ${PORT}`)
 })
